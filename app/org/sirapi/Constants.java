e0bca97cb31dc12b12d96fb96383e35cb31f8e05
Paulo Pinheiro <pp3223@gmail.com>
2023-08-17 13:58:06 +0100

    Update Constants.java

--- app/org/sirapi/Constants.java
+++ app/org/sirapi/Constants.java
@@ -2,7 +2,7 @@ package org.sirapi;
 
 public class Constants {
 
-    public static final String REPOSITORY_VERSION = "0.4";
+    public static final String REPOSITORY_VERSION = "0.5";
 
     public static final String FLASH_MESSAGE_KEY = "message";
     public static final String FLASH_ERROR_KEY = "error";
