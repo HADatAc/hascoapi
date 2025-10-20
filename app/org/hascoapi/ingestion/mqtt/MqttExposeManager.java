fc8c337953e670c004e867d1bf8406bcfea9c8ef
Tiago Moreira <tiago.moreira@ipiaget.pt>
2025-10-20 11:33:26 +0100

    update

--- app/org/hascoapi/ingestion/mqtt/MqttExposeManager.java
+++ app/org/hascoapi/ingestion/mqtt/MqttExposeManager.java
@@ -7,6 +7,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class MqttExposeManager {
 
