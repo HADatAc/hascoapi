305462686fcebc92fa9027230fdd50bbcc8b6815
PauloPinheiro <pp3223@gmail.com>
2025-11-10 13:11:06 +0000

    verifying IngestAPI

--- app/org/hascoapi/console/controllers/restapi/IngestionAPI.java
+++ app/org/hascoapi/console/controllers/restapi/IngestionAPI.java
@@ -469,6 +469,8 @@ public class IngestionAPI extends Controller {
             System.out.println(errorMsg);
             return ok(ApiUtil.createResponse(errorMsg,false));
         }
+        System.out.println("Datafileuri: " + datafileuri);
+        System.out.println("Filename: " + filename);
         switch (elementtype) {
             case "ins":
                 INSGen.genByStatus(status,filename);
