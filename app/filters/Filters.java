02d3b3d6172709ad056a68dd21c10aeef08f9d57
PauloPinheiro <pp3223@gmail.com>
2026-04-30 02:37:27 +0100

    feat: add CORS filter to allow cross-origin requests
    - Add CORSFilter to Filters.java
    - Configure CORS in application.conf
    - Allow origins: localhost:8086, 8099, 8082, 8081
    - Required for frontend equipment capability API calls
    - Fixes CORS policy blocking errors

--- app/filters/Filters.java
+++ app/filters/Filters.java
@@ -1,6 +1,7 @@
 package filters;
 
 import org.pac4j.play.filters.SecurityFilter;
+import play.filters.cors.CORSFilter;
 import play.http.HttpFilters;
 import play.mvc.EssentialFilter;
 
@@ -11,14 +12,16 @@ import java.util.List;
 public class Filters implements HttpFilters {
 
     private final SecurityFilter securityFilter;
+    private final CORSFilter corsFilter;
 
     @Inject
-    public Filters(SecurityFilter securityFilter) {
+    public Filters(SecurityFilter securityFilter, CORSFilter corsFilter) {
         this.securityFilter = securityFilter;
+        this.corsFilter = corsFilter;
     }
 
     @Override
     public List<EssentialFilter> getFilters() {
-        return Arrays.asList(securityFilter.asJava());
+        return Arrays.asList(corsFilter.asJava(), securityFilter.asJava());
     }
 }
