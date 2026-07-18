e61d3d7006939e7175c6e95a111555e7894f7768
PauloPinheiro <pp3223@gmail.com>
2026-07-18 20:09:09 +0100

    Add statistics API endpoints for instruments, procedures, and anatomical structures counts

---
+++ app/org/hascoapi/console/controllers/restapi/StatisticsAPI.java
@@ -0,0 +1,124 @@
+package org.hascoapi.console.controllers.restapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSetRewindable;
+import org.hascoapi.utils.ApiUtil;
+import org.hascoapi.utils.CollectionUtil;
+import org.hascoapi.utils.NameSpaces;
+import org.hascoapi.utils.SPARQLUtils;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * API Controller for statistics queries
+ */
+public class StatisticsAPI extends Controller {
+
+    /**
+     * Get count of instruments (subclasses of vstoi:Instrument + 1)
+     * GET /hascoapi/api/statistics/instruments/count
+     */
+    public Result getInstrumentsCount() {
+        int count = countInstruments();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode();
+        result.put("total", count);
+        return ok(ApiUtil.createResponse(result, true));
+    }
+
+    /**
+     * Get count of clinical procedures (NCIT procedure classes)
+     * GET /hascoapi/api/statistics/procedures/count
+     */
+    public Result getClinicalProceduresCount() {
+        int count = countClinicalProcedures();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode();
+        result.put("total", count);
+        return ok(ApiUtil.createResponse(result, true));
+    }
+
+    /**
+     * Get count of anatomical structures (UBERON classes)
+     * GET /hascoapi/api/statistics/anatomy/count
+     */
+    public Result getAnatomicalStructuresCount() {
+        int count = countAnatomicalStructures();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode();
+        result.put("total", count);
+        return ok(ApiUtil.createResponse(result, true));
+    }
+
+    /**
+     * Count instruments: number of subclasses of vstoi:Instrument + 1
+     */
+    private int countInstruments() {
+        String queryString = NameSpaces.getInstance().printSparqlNameSpaceList() +
+                "SELECT (COUNT(DISTINCT ?class) as ?count) WHERE { " +
+                "   ?class rdfs:subClassOf* vstoi:Instrument . " +
+                "} ";
+
+        ResultSetRewindable resultsrw = SPARQLUtils.select(
+                CollectionUtil.getCollectionPath(CollectionUtil.Collection.SPARQL_QUERY), queryString);
+
+        if (resultsrw.hasNext()) {
+            QuerySolution soln = resultsrw.next();
+            return soln.getLiteral("count").getInt();
+        }
+        return 0;
+    }
+
+    /**
+     * Count clinical procedures from NCIT ontology
+     * Counts classes that are subclasses of pmsr:MedicalSimulationProcessStem
+     */
+    private int countClinicalProcedures() {
+        String queryString = NameSpaces.getInstance().printSparqlNameSpaceList() +
+                "SELECT (COUNT(DISTINCT ?class) as ?count) WHERE { " +
+                "   { " +
+                "     ?class rdfs:subClassOf* <http://pmsr.net/ont/pmsr#MedicalSimulationProcessStem> . " +
+                "   } UNION { " +
+                "     ?class a owl:Class . " +
+                "     ?class rdfs:subClassOf* <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C18020> . " +
+                "   } " +
+                "} ";
+
+        ResultSetRewindable resultsrw = SPARQLUtils.select(
+                CollectionUtil.getCollectionPath(CollectionUtil.Collection.SPARQL_QUERY), queryString);
+
+        if (resultsrw.hasNext()) {
+            QuerySolution soln = resultsrw.next();
+            return soln.getLiteral("count").getInt();
+        }
+        return 0;
+    }
+
+    /**
+     * Count anatomical structures from UBERON ontology
+     * Counts all UBERON classes
+     */
+    private int countAnatomicalStructures() {
+        String queryString = NameSpaces.getInstance().printSparqlNameSpaceList() +
+                "SELECT (COUNT(DISTINCT ?class) as ?count) WHERE { " +
+                "   ?class a owl:Class . " +
+                "   FILTER(STRSTARTS(STR(?class), \"http://purl.obolibrary.org/obo/UBERON_\")) " +
+                "} ";
+
+        ResultSetRewindable resultsrw = SPARQLUtils.select(
+                CollectionUtil.getCollectionPath(CollectionUtil.Collection.SPARQL_QUERY), queryString);
+
+        if (resultsrw.hasNext()) {
+            QuerySolution soln = resultsrw.next();
+            return soln.getLiteral("count").getInt();
+        }
+        return 0;
+    }
+}
